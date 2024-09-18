24a0e2b43e8cf635c529c1a3c5156fa178267cdf
Lexi Grant <106564286+lexigrant@users.noreply.github.com>
2024-09-18 12:07:55 -0600

    Time: 82 ms (23.84%) | Memory: 53.6 MB (90.68%) - LeetSync

---
+++ 2502-sort-the-people/sort-the-people.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string[]} names
+ * @param {number[]} heights
+ * @return {string[]}
+ */
+var sortPeople = function(names, heights) {
+  const people = names.map((name, i) => ({name, height: heights[i]}));
+  
+  people.sort((a, b) => b.height - a.height);
+    
+  return people.map(person => person.name)  
+};
